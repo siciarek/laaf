ee05d5ebeb83b900c2e10e5d644cde11323797fe
siciarek <siciarek@gmail.com>
2012-10-11 22:31:36 +0200

    adds dispatcher and exceptions

--- web/index.php
+++ web/index.php
@@ -9,10 +9,74 @@ $data["repo"]   = "https://github.com/siciarek/laaf";
 $data["server"] = $_SERVER;
 
 $frame  = LAAF_Frame::getInfo($msg, $data);
+
+$request = file_get_contents("php://input");
+
+$reader = new LAAF_Reader_Xml();
 $writer = new LAAF_Writer_Xml();
 
 if (array_key_exists("CONTENT_TYPE", $_SERVER) and $_SERVER["CONTENT_TYPE"] === "application/json") {
+    $reader = new LAAF_Reader_Json();
     $writer = new LAAF_Writer_Json();
+}
+/*
+
+errors:
+
+""
+
+"ABC"
+
+"<x>ABC</x>"
+// no ns
+"<laaf:frame>
+<laaf:success>1</laaf:success>
+
+</laaf:frame>"
+
+'<?xml version="1.0" encoding="UTF-8"?>
+<laaf:frame xmlns:laaf="http://laaf.siciarek.pl">
+    <laaf:success>1</laaf:success>
+    <laaf:type>request</laaf:type>
+    <laaf:datetime>2012-10-11T18:27:33</laaf:datetime>
+    <laaf:msg>UsersList</laaf:msg>
+    <laaf:data/>
+</laaf:frame>'
+
+<laaf:frame xmlns:laaf="http://laaf.siciarek.pl">
+    <laaf:success>1</laaf:success>
+    <laaf:type>request</laaf:type>
+    <laaf:datetime>2012-10-11T18:27:33</laaf:datetime>
+    <laaf:msg>UserDetails</laaf:msg>
+    <laaf:data>
+        <laaf:id>45</laaf:id>
+    </laaf:data>
+</laaf:frame>
+
+*/
+
+if(!empty($request)) {
+
+    try {
+        $input = $reader->read($request);
+
+        $msg = "Your request is valid";
+        $data = array(
+            "request" => $input
+        );
+
+        $dispatcher = new LAAF_Dispatcher();
+        $frame  = $dispatcher->assignService($input["msg"], $input["data"]);
+    }
+    catch(Exception $e) {
+        $msg = strip_tags($e->getMessage());
+        $data = array();
+        $data["message"] = $msg;
+        $data["code"] = $e->getCode();
+        $data["file"] = $e->getFile();
+        $data["line"] = $e->getLine();
+        $frame  = LAAF_Frame::getError($msg, $data);
+    }
 }
 
 header("Content-type: " . $writer->getMimeType());
