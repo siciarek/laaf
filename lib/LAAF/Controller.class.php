34693f45884867381b1fe6d0889ba28bbc4c44f5
siciarek <siciarek@gmail.com>
2012-10-13 00:25:18 +0200

    Controller added

---
+++ lib/LAAF/Controller.class.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: jsiciarek
+ * Date: 12.10.12
+ * Time: 16:54
+ * To change this template use File | Settings | File Templates.
+ */
+/**
+ * Controller class
+ * @package LAAF
+ */
+class LAAF_Format
+{
+    private $formats = array();
+
+    public function __construct()
+    {
+        $this->formats = array(
+            "application/json" => function ($input) {
+                return json_decode($input) !== null;
+            },
+            "application/xml"  => function ($input) {
+                $dom = new DOMDocument();
+                @$dom->loadXml($input);
+                $error = error_get_last();
+                return $error === null;
+            }
+        );
+    }
+
+    public function recognize($input)
+    {
+        foreach ($this->formats as $mimetype => $function) {
+            if ($function($input) === true) {
+                return $mimetype;
+            }
+        }
+
+        return null;
+    }
+
+}
+
+class LAAF_Controller
+{
+    public static function actionIndex($request)
+    {
+        if (!empty($request)) {
+
+            $format   = new LAAF_Format();
+            $mimetype = $format->recognize($request);
+
+            try {
+                if ($mimetype === null) {
+                    throw new Exception("Invalid request format");
+                }
+
+                $reader = new LAAF_Reader_Xml();
+                $writer = new LAAF_Writer_Xml();
+
+                if($mimetype === "application/json") {
+                    $reader = new LAAF_Reader_Json();
+                    $writer = new LAAF_Writer_Json();
+                }
+
+                $input = $reader->read($request);
+                $dispatcher = new LAAF_Dispatcher();
+                $frame  = $dispatcher->assignService($input["msg"], $input["data"]);
+                $output = $writer->format($frame);
+                $mimetype = $writer->getMimeType();
+
+
+            } catch (Exception $e) {
+                $msg             = strip_tags($e->getMessage());
+                $data            = array();
+                $data["message"] = $msg;
+                $data["code"]    = $e->getCode();
+                $data["file"]    = $e->getFile();
+                $data["line"]    = $e->getLine();
+
+                $errframe = LAAF_Frame::getError($msg, $data);
+                $writer   = new LAAF_Writer_Xml();
+                $output   = $writer->format($errframe, false);
+                $mimetype = $writer->getMimeType();
+
+            }
+        } else {
+            $msg = "LAAF - Light As A Feather Webservice Protocol";
+
+            $data["author"] = "Jacek Siciarek <siciarek@gmail.com>";
+            $data["repo"]   = "https://github.com/siciarek/laaf";
+            $data["server"] = $_SERVER;
+
+            $frame  = LAAF_Frame::getInfo($msg, $data);
+            $writer = new LAAF_Writer_Xml();
+            $output = $writer->format($frame, false);
+            $mimetype = $writer->getMimeType();
+        }
+
+
+        header("Content-type: " . $mimetype);
+        echo $output;
+        exit;
+    }
+}
---
+++ test/lib/LAAF/ControllerTest.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Controller tests
+ */
+class ControllerTest extends PHPUnit_Framework_TestCase
+{
+    private static $service_url = "http://laaf.dev";
+
+    function controllerXmlRequestsProvider()
+    {
+        return array(
+            array(
+                file_get_contents(FIXTURES_DIR . '/user-list-request.xml'),
+                file_get_contents(FIXTURES_DIR . '/user-list-response.xml'),
+            ),
+            array(
+                file_get_contents(FIXTURES_DIR . '/user-details-request.xml'),
+                file_get_contents(FIXTURES_DIR . '/user-details-response.xml'),
+            ),
+        );
+    }
+
+    function controllerJsonRequestsProvider()
+    {
+        return array(
+            array(
+                file_get_contents(FIXTURES_DIR . '/user-list-request.json'),
+                file_get_contents(FIXTURES_DIR . '/user-list-response.json'),
+            ),
+            array(
+                file_get_contents(FIXTURES_DIR . '/user-details-request.json'),
+                file_get_contents(FIXTURES_DIR . '/user-details-response.json'),
+            ),
+        );
+    }
+
+    /**
+     * @dataProvider controllerXmlRequestsProvider
+     * @param $request
+     * @param $expected
+     */
+    function testControllerXmlRequests($request, $expected)
+    {
+        $w     = new LAAF_Writer_Xml();
+        $given = "GIVEN";
+
+        try {
+            $given = $this->curl->post(self::$service_url, $request);
+            $given = preg_replace("|(<laaf:datetime)>.*?(</laaf:datetime>)|", '$1' . '>' . $this->dtxml . '$2', $given);
+        } catch (Exception $e) {
+            $this->assertTrue(false, $e->getMessage());
+        }
+
+        $this->assertEquals($expected, $given);
+    }
+
+    /**
+     * @dataProvider controllerJsonRequestsProvider
+     * @param $request
+     * @param $expected
+     */
+    function testControllerJsonRequests($request, $expected)
+    {
+        $w     = new LAAF_Writer_Json();
+        $given = "GIVEN";
+        $expected = json_encode(json_decode($expected, true));
+
+        try {
+            $given = $this->curl->post(self::$service_url, $request);
+            $given = preg_replace('|("datetime":)"[^"]+"|', '$1"' . $this->datetime . '"', $given);
+        } catch (Exception $e) {
+            $this->assertTrue(false, $e->getMessage());
+        }
+
+        $this->assertEquals($expected, $given);
+    }
+
+    function setUp()
+    {
+        $this->curl     = new MyCurl();
+        $this->datetime = "1966-10-21 15:10:00";
+        $this->dtxml    = "1966-10-21T15:10:00";
+    }
+}
