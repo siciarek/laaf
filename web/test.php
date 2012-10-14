4e6fbdb0da84db0f2f50a86334725b3e80c01415
siciarek <siciarek@gmail.com>
2012-10-14 23:39:31 +0200

    Set up http server for tests CGI fix

--- web/test.php
+++ web/test.php
@@ -1,19 +1,11 @@
 #!/usr/bin/env php
 <?php
-echo "LAAF\n";
-echo "LAAF\n";
-echo "LAAF\n";
-echo "LAAF\n";
-echo "LAAF\n";
-echo "LAAF\n";
-echo "LAAF\n";
-echo "LAAF\n";
-echo "LAAF\n";
-echo "LAAF\n";
-echo "LAAF\n";
-echo "LAAF\n";
-echo "LAAF\n";
-echo "LAAF\n";
 
-phpinfo();
-?>
+include_once __DIR__ . '/../lib/LAAF/bootstrap.php';
+
+$request = file_get_contents("php://input");
+$controller = new LAAF_Controller();
+$response = $controller->actionIndex($request);
+
+echo "Content-type: " . $response["mimetype"] . "\n\n";
+echo $response["output"];
