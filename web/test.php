079b34d82cd757d1782c862b4d1c2f9e1550a23c
siciarek <siciarek@gmail.com>
2012-10-15 01:12:49 +0200

    Set up http server for tests CGI fix

--- web/test.php
+++ web/test.php
@@ -1,11 +1,9 @@
 #!/usr/bin/env php
 <?php
 
-// #!/usr/local/php5.3/bin/php
-
 @include_once __DIR__ . '/../lib/LAAF/bootstrap.php';
 
-$request = file_get_contents("php://stdin", false, null, 0, $_SERVER["CONTENT_LENGTH"]);
+$request = file_get_contents("php://stdin", false, null, 0, intval($_SERVER["CONTENT_LENGTH"]));
 
 $controller = new LAAF_Controller();
 $response = $controller->actionIndex($request);
