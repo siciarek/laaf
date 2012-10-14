507009c0d27083eecb0973248c4f253a691b0247
siciarek <siciarek@gmail.com>
2012-10-15 01:48:07 +0200

    Travis setup final fix

--- web/index.php
+++ web/index.php
@@ -1,10 +1,15 @@
 <?php
 
+$request = file_get_contents("php://input");
+
 @include_once __DIR__ . '/../lib/LAAF/bootstrap.php';
 
-$request = file_get_contents("php://input");
 $controller = new LAAF_Controller();
 $response = $controller->actionIndex($request);
 
-header("Content-type: " . $response["mimetype"]);
-echo $response["output"];
+$header = "Content-type: " . $response["mimetype"];
+$output = $response["output"];
+
+
+header($header);
+print($output);
--- web/travis.php
+++ web/travis.php
@@ -1,13 +1,15 @@
 #!/usr/bin/env php
 <?php
-// #!/usr/local/php5.3/bin/php
+$request = file_get_contents("php://stdin", false, null, 0, intval($_SERVER["CONTENT_LENGTH"]));
 
 @include_once __DIR__ . '/../lib/LAAF/bootstrap.php';
-
-$request = file_get_contents("php://stdin", false, null, 0, intval($_SERVER["CONTENT_LENGTH"]));
 
 $controller = new LAAF_Controller();
 $response = $controller->actionIndex($request);
 
-echo "Content-type: " . $response["mimetype"] . "\n\n";
-echo $response["output"];
+$header = "Content-type: " . $response["mimetype"];
+$output = $response["output"];
+
+
+print($header);
+print($output);
