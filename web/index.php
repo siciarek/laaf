cfd01785a3ca4db5d15bc646110ff6a1c7d3f6a4
siciarek <siciarek@gmail.com>
2012-10-15 02:00:38 +0200

    Travis setup final fix

--- web/index.php
+++ web/index.php
@@ -2,14 +2,7 @@
 
 $request = file_get_contents("php://input");
 
-@include_once __DIR__ . '/../lib/LAAF/bootstrap.php';
-
-$controller = new LAAF_Controller();
-$response = $controller->actionIndex($request);
-
-$header = "Content-type: " . $response["mimetype"];
-$output = $response["output"];
-
+include_once __DIR__ . '/index.inc';
 
 header($header);
 print($output);
--- web/travis.php
+++ web/travis.php
@@ -2,14 +2,7 @@
 <?php
 $request = file_get_contents("php://stdin", false, null, 0, intval($_SERVER["CONTENT_LENGTH"]));
 
-@include_once __DIR__ . '/../lib/LAAF/bootstrap.php';
-
-$controller = new LAAF_Controller();
-$response = $controller->actionIndex($request);
-
-$header = "Content-type: " . $response["mimetype"];
-$output = $response["output"];
-
+include_once __DIR__ . '/index.inc';
 
 print($header . "\n\n");
 print($output);
